7203ebcc851e73d81a788be10549bd6e0854754f
Nguyễn Phắc <phapdeptrai2k2@gmail.com>
2022-12-19 12:11:19 +0700

    add remove helper

--- Services/DBUpdater.cs
+++ Services/DBUpdater.cs
@@ -1,4 +1,5 @@
 ﻿using ClinicManagement.DbContexts;
+using ClinicManagement.Models;
 using System.Security.Cryptography;
 
 namespace ClinicManagement.Services
@@ -10,6 +11,74 @@ namespace ClinicManagement.Services
         public DBUpdater(ClinicDbContextFactory dbContextFactory)
         {
             _dbContextFactory = dbContextFactory;
+        }
+
+        public async Task RemoveMedicine(Guid medicineId)
+        {
+            using (ClinicDbContext dbContext = _dbContextFactory.CreateDbContext())
+            {
+                var obj = dbContext.Medicines.FirstOrDefault(p => p.Id == medicineId);
+                if (obj != null)
+                {
+                    dbContext.Medicines.Remove(obj);
+                    await dbContext.SaveChangesAsync();
+                }
+            }
+        }
+
+        public async Task RemovePatient(string patientId)
+        {
+            using (ClinicDbContext dbContext = _dbContextFactory.CreateDbContext())
+            {
+                var obj = dbContext.Patients.FirstOrDefault(p => p.Id == patientId);
+                if (obj != null)
+                {
+                    dbContext.Patients.Remove(obj);
+                    await dbContext.SaveChangesAsync();
+                };
+            }
+        }
+
+        public async Task RemoveUnit(Guid unitId)
+        {
+            using (ClinicDbContext dbContext = _dbContextFactory.CreateDbContext())
+            {
+                var obj = dbContext.Units.FirstOrDefault(p => p.Id == unitId);
+                if (obj != null)
+                {
+                    dbContext.Units.Remove(obj);
+                    await dbContext.SaveChangesAsync();
+                };
+            }
+        }
+
+        public async Task UpdateMedicine(Medicine medicine)
+        {
+            using (ClinicDbContext dbContext = _dbContextFactory.CreateDbContext())
+            {
+                var obj = dbContext.Medicines.FirstOrDefault(p => p.Id == medicine.Id);
+                if (obj != null)
+                {
+                    obj.Price = medicine.Price;
+                    obj.Stock = medicine.Stock;
+                    obj.Unit.Id = medicine.UnitId;
+                    obj.Name = medicine.Name;
+                    dbContext.Medicines.Update(obj);
+                    await dbContext.SaveChangesAsync();
+                }
+            }
+        }
+
+
+        public async Task UpdateMethod(Method method)
+        {
+            using (ClinicDbContext dbContext = _dbContextFactory.CreateDbContext())
+            {
+                var obj = dbContext.Methods.FirstOrDefault(p => p.Id == method.Id);
+                obj.Name = method.Name;
+                dbContext.Methods.Update(obj);
+                await dbContext.SaveChangesAsync();
+            }
         }
 
         public async Task UpdateParameters(int medicalCost, int maxPatients)
@@ -45,6 +114,35 @@ namespace ClinicManagement.Services
                 var param = dbContext.Parameters.SingleOrDefault(p => p.Id == "1");
                 param.Password = newpass;
                 dbContext.Parameters.Update(param);
+                await dbContext.SaveChangesAsync();
+            }
+        }
+
+        public async Task UpdatePatient(Patient patient)
+        {
+            using (ClinicDbContext dbContext = _dbContextFactory.CreateDbContext())
+            {
+                var obj = dbContext.Patients.FirstOrDefault(p => p.Id == patient.Id);
+                if (obj != null)
+                {
+                    obj.Fullname = patient.Fullname;
+                    obj.Address = patient.Address;
+                    obj.Gender = patient.Gender;
+                    obj.Dob = patient.Dob;
+
+                    dbContext.Patients.Update(obj);
+                    await dbContext.SaveChangesAsync();
+                }
+            }
+        }
+
+        public async Task UpdateUnit(Unit unit)
+        {
+            using (ClinicDbContext dbContext = _dbContextFactory.CreateDbContext())
+            {
+                var obj = dbContext.Units.FirstOrDefault(p => p.Id == unit.Id);
+                obj.Name = unit.Name;
+                dbContext.Units.Update(obj);
                 await dbContext.SaveChangesAsync();
             }
         }
--- Services/IDataUpdater.cs
+++ Services/IDataUpdater.cs
@@ -1,8 +1,17 @@
-﻿namespace ClinicManagement.Services
+﻿using ClinicManagement.Models;
+
+namespace ClinicManagement.Services
 {
     public interface IDataUpdater
     {
         public Task UpdateParameters(int medicalCost, int maxPatients);
         public Task UpdatePassword(string password);
+        public Task UpdatePatient(Patient patient);
+        public Task UpdateMedicine(Medicine medicine);
+        public Task UpdateUnit(Unit unit);
+        public Task UpdateMethod(Method method);
+        public Task RemovePatient(string patientId);
+        public Task RemoveMedicine(Guid medicineId);
+        public Task RemoveUnit(Guid unitId);
     }
 }
